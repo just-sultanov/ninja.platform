5c67a34e5e65ed1cb6175e92b26e83a8cef4dcfb
Ilshat Sultanov <just.sultanov@gmail.com>
2021-03-14 13:17:16 +0300

    [ninja.platform/javac] Add helpers to calculate a classpath

--- javac/src/main/clojure/ninja/javac.clj
+++ javac/src/main/clojure/ninja/javac.clj
@@ -1,6 +1,41 @@
-(ns ninja.javac)
+(ns ninja.javac
+  (:refer-clojure :exclude [compile])
+  (:gen-class)
+  (:require
+    [clojure.java.io :as io]
+    [clojure.tools.deps.alpha :as deps]
+    [clojure.tools.deps.alpha.util.maven :as maven])
+  (:import
+    (java.nio.file
+      Path
+      Paths)))
 
 
-(defn square
-  [x]
-  (* x x))
+(set! *warn-on-reflection* true)
+
+
+(defn ^Path make-path
+  "Converts a path string, or a sequence of strings that when joined form a path string, to a Path.
+  Returns an instance of `java.nio.file.Path`"
+  [path & paths]
+  (->> paths
+    (map str)
+    (into-array String)
+    (Paths/get (str path))))
+
+
+(defn make-classpath
+  "Returns a calculated classpath taken from deps."
+  ([]
+    (make-classpath {}))
+
+  ([{:keys [deps-map aliases]}]
+    (let [deps-map (or deps-map (deps/slurp-deps (io/file "deps.edn")))
+          deps-map (update deps-map :mvn/repos #(merge maven/standard-repos %))
+          paths    (select-keys deps-map [:paths])
+          args-map (deps/combine-aliases deps-map aliases)]
+      (as-> deps-map $
+        (deps/resolve-deps $ args-map)
+        (deps/make-classpath-map paths $ args-map)
+        (get $ :classpath-roots)
+        (deps/join-classpath $)))))
--- javac/src/test/clojure/ninja/javac_test.clj
+++ javac/src/test/clojure/ninja/javac_test.clj
@@ -1,9 +1,25 @@
 (ns ninja.javac-test
   (:require
+    [clojure.string :as string]
     [clojure.test :refer [deftest testing is]]
     [ninja.javac :as sut]))
 
 
-(deftest ^:unit square-test
-  (testing "dummy test"
-    (is (= 4 (sut/square 2)))))
+(deftest make-path-test
+  (testing "build path"
+    (is (= "a" (str (sut/make-path "a"))))
+    (is (= "a/b/c" (str (sut/make-path "a/b" "c")) (str (sut/make-path "a" "b" "c"))))))
+
+
+(deftest make-classpath-test
+  (testing "calculate classpath"
+    (let [includes? #(string/includes? % "ninja/platform/response/0.1.0/response-0.1.0.jar")]
+      (testing "without any arguments"
+        (is (false? (includes? (sut/make-classpath))))
+        (is (false? (includes? (sut/make-classpath {:deps-map nil}))))
+        (is (false? (includes? (sut/make-classpath {:deps-map {}})))))
+
+      (testing "with provided aliases"
+        (is (false? (includes? (sut/make-classpath {:deps-map {}, :aliases [:module.test/deps]}))))
+        (is (true? (includes? (sut/make-classpath {:deps-map nil, :aliases [:module.test/deps]}))))
+        (is (true? (includes? (sut/make-classpath {:aliases [:module.test/deps]}))))))))
